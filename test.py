f22b5b03f1c91740627204a83726b39d0700c65b
Gabriel Rovina <gabriel.rovina@gmail.com>
2019-11-11 11:38:44 +0000

    Adjust test script and add quotation samples

--- test.py
+++ test.py
@@ -3,28 +3,10 @@ import gzip
 import os
 import subprocess
 import shutil
+import sys
+import webbrowser
 
-docs = [
-    '''“At its heart, politics isn’t about exchanges across these dispatch boxes, nor about eloquent speeches or media headlines. Politics is about the difference we make, every day, to the lives of people up and down this country. They are our reason for being here, and we should never forget it,” May told the Commons at PMQs.''',
 
-    '''“Oh Whit my heart breaks for you. Sending you my love,” one Instagram user wrote.
-“This is a topic that so rarely is talked about,” wrote another. “So many women experience miscarriages and we are felt alone because we think we are the only ones who have gone thru it.. thank you for bringing this up and being raw honest about it.”''',
-
-    '''Ms Davidson also dismissed speculation that the Scottish Conservatives could become independent from the wider UK Conservative Party.
-She said: "We are a devolved party, just as Scotland is devolved."
-"As Scottish leader, I am in charge of policy, campaigning, staffing, funding, management, candidate selection and everything else that goes with fighting elections and communicating with voters."
-"But my message is simple: not on my watch."''',
-
-    '''Colin Clement, chairman of Stobswell Forum, said they are working with Dundee City Council to install the systems and that is what was causing the delay.''',
-
-    '''Conservative MP Helen Whately, who raised the case, argued that flexible working should be the default position for all employees and should be added to all job adverts.''',
-
-    '''Boris Johnson has claimed that "a sensible, pragmatic Brexit" will cement Scotland's place within the UK, as he sought to position himself as a firm defender of the Union while on a visit north of the Border.''',
-
-    '''In a statement, the Official Receiver said he was "encouraged by the level of interest shown in purchasing British Steel" and that he was "now in further discussions with the potential buyers who have made the most viable offers for the business".''',
-
-    '''The anti-abortion campaign group Both Lives Matter welcomed Hunt and Johnson's position because, it claimed, the majority in Northern Ireland "don't want abortion laws imposed by Westminster".'''
-]
 input_dir = 'input'
 output_dir = 'output'
 jar_path = 'target/qsample-0.1-jar-with-dependencies.jar'
@@ -38,7 +20,12 @@ def clear_folders():
     os.makedirs(output_dir)
 
 
-def write_docs():
+def read_docs(path):
+    with open(path) as f:
+        return [l.strip() for l in f]
+
+
+def write_docs(docs):
     print('reading %d examples' % len(docs))
     for i, doc in enumerate(docs):
         filename = '%02d.txt' % i
@@ -90,11 +77,17 @@ def parse_results():
     print('wrote results to %s' % output_html)
 
 
+def open_chrome():
+    webbrowser.open_new_tab(output_html)
+
+
 def main():
+    docs = read_docs(sys.argv[1])
     clear_folders()
-    write_docs()
+    write_docs(docs)
     run_qsample()
     parse_results()
+    open_chrome()
 
 
 if __name__ == '__main__':
